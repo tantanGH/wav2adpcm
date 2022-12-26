7e4a081ed9b77f3e23def96cfec5a37e07a25f85
tantanGH <tantangh68@gmail.com>
2022-12-26 22:52:35 +0900

    doc update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # wav2adpcm
 Wave format PCM data to X68000 ADPCM data converter in Python
+
+### Install
+
+    pip install git+https://github.com/tantanGH/wav2adpcm.git
+
+### Usage
+
+    wav2adpcm [options] <input-wave-file> <output-adpcm-file>
+
+Note that you need to add wav2adpcm installed folder to your PATH environment variable.
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wav2adpcm",
+    version="0.0.1",
+    author="tantanGH",
+    author_email="tantanGH@github",
+    license='MIT',
+    description="WAVE format file to X68000 ADPCM file converter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tantanGH/wav2adpcm",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'wav2adpcm=wav2adpcm.wav2adpcm:main'
+        ]
+    },
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    setup_requires=["pydub"],
+)
